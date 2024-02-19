32e1acf99846e5afde8d39e0136c89fa24e79446
simone.leveghi <simone.leveghi@buonarroti.tn.it>
2024-02-19 10:00:47 +0100

    Add boilerplate

--- src/Azienda.java
+++ src/Azienda.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
 public class Azienda {
+    private String nome;
+    private Date dataCreazione;
+    private ArrayList<Dipendente> dipendenti;
+
+    public Azienda(String nome, Date dataCreazione) {
+        this.nome = nome;
+        this.dataCreazione = new Date(dataCreazione.getTime());
+        this.dipendenti = new ArrayList<Dipendente>();
+    }
+
+    public Azienda(String nome, Date dataCreazione, ArrayList<Dipendente> dipendenti) {
+        this(nome, dataCreazione);
+
+        this.dipendenti = new ArrayList<Dipendente>();
+        for(int i = 0; i < dipendenti.size(); ++i) {
+            this.dipendenti.add(dipendenti.get(i).clone());
+        }
+    }
+
+    public Azienda(String nome, Date dataCreazione, Dipendente[] dipendenti) {
+        this(nome, dataCreazione, (ArrayList<Dipendente>) Arrays.asList(dipendenti));
+    }
 }
